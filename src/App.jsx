559d75e44838c7516315ede1b2ac3aefddf6cfbf
Newton Ombese <newtonombese1@gmail.com>
2023-12-12 06:28:08 +0300

    Add login & signup routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import { Routes, Route } from 'react-router-dom'
 import Main from './components/Main'
 import Detail from './components/Details'
 import Reservation from './components/Reservations'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Main/>}></Route>
     <Route path="/details/:id" element={<Detail/>}></Route>
     <Route path="/reservations" element={<Reservation/>}></Route>
+    <Route path="/login" element={<Login />}></Route>
+    <Route path='/signup' element={<Signup />} />
   </Routes>
 )
 

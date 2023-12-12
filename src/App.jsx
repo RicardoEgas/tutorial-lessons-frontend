578a17f83ffbd26fc7678fd7e5bb26e59f722299
Newton Ombese <newtonombese1@gmail.com>
2023-12-12 18:03:00 +0300

    Import splash screen to the app component

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,20 @@ import Reservation from './components/Reservations'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
 import ResetPassword from './components/auth/ResetPassword'
+import SplashScreen from './components/splash-screen/SplashScreen'
 
 const App = () => (
-  <Routes>
-    <Route path="/" element={<Main/>}></Route>
-    <Route path="/details/:id" element={<Detail/>}></Route>
-    <Route path="/reservations" element={<Reservation/>}></Route>
-    <Route path="/login" element={<Login />}></Route>
-    <Route path='/signup' element={<Signup />} />
-    <Route path="/reset-password" element={<ResetPassword />}></Route>
-  </Routes>
+  <>
+    <SplashScreen />
+    <Routes>
+      <Route path="/" element={<Main/>}></Route>
+      <Route path="/details/:id" element={<Detail/>}></Route>
+      <Route path="/reservations" element={<Reservation/>}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path='/signup' element={<Signup />} />
+      <Route path="/reset-password" element={<ResetPassword />}></Route>
+    </Routes>
+  </>
 )
 
 export default App

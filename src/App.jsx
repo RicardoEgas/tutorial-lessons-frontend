e007055c66f88301a0ec4fe31e652941b6220d92
RicardoEgas <rj_egas@hotmail.com>
2023-12-13 15:12:19 +0100

    Add classes page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom'
 import Main from './components/Main'
 import Detail from './components/Details'
 import Reservation from './components/Reservations'
+import AddClassForm from './components/Classes'
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Main/>}></Route>
     <Route path="/details/:id" element={<Detail/>}></Route>
     <Route path="/reservations" element={<Reservation/>}></Route>
+    <Route path="/classes" element={<AddClassForm/>}></Route>
   </Routes>
 )
 

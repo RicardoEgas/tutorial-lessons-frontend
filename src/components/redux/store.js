d40c348ea062f06dc9ddb00ba68d7d65bced03d3
Newton Ombese <newtonombese1@gmail.com>
2023-12-16 07:55:16 +0300

    Include reservation to my store

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from 'redux-logger';
 import authReducer from './authStore/authSlice';
+import reservationReducer from './reservationStore/reservationSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    reservation: reservationReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })

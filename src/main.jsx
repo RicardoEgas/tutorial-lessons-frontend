5a9ad2a54354102d0df2bc743173097e34a56cff
Newton Ombese <newtonombese1@gmail.com>
2023-12-16 07:07:37 +0300

    Linking up React and Redux using Provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import Navbar from './components/Navbar.jsx';
+import { Provider } from 'react-redux';
+import store from './components/redux/store.js';
+// import Navbar from './components/Navbar.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-        <Navbar />
+      <Provider store={store}>
+        {/* <Navbar /> */}
         <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
